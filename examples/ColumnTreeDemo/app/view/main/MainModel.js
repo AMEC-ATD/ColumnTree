7a7978ab20be33d8e4ea0f8d0d7c1fae25bbde83
Andy Lumb <andy.lumb@gmail.com>
2014-10-08 12:16:48 -0700

    Added example app.
    sencha generate app -ext ColumnTreeDemo ColumnTreeDemo

---
+++ examples/ColumnTreeDemo/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('ColumnTreeDemo.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'ColumnTreeDemo'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
